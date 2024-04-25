4288b492cc5a1088a9d38b3670e589b33f6c6b9e
Soohyun Bae <161088334+sbpodonos@users.noreply.github.com>
2024-04-25 15:40:20 +0900

    Check the min, recommended, required versions
    Check the min, recommended, required versions

--- podonos/version.py
+++ podonos/version.py
@@ -7,6 +7,7 @@ import importlib.metadata
 import logging
 from packaging.version import Version
 import requests
+from typing import Tuple
 
 from podonos.constant import bcolors
 
@@ -14,31 +15,49 @@ from podonos.constant import bcolors
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-def get_min_required_version(api_url):
-    """Gets the minimum required version of this package.
+
+def get_versions(api_url: str) -> Tuple[str, str, str]:
+    """Gets the minimum, recommended, and latest versions of this package.
 
     Returns:
-        The minimum required version strong.
+        {min, recommended, latest} versions
     """
-    response = requests.get(f'{api_url}/version/min-required')
+    response = requests.get(f'{api_url}/version/sdk')
     if response.status_code != 200:
         raise requests.exceptions.HTTPError
-    required_version = response.text.replace('"', '')
-    return required_version
+    version_json = response.json()
+    min_version = version_json.get('minimum')
+    recommended_version = version_json.get('recommended')
+    latest_version = version_json.get('latest')
+    return min_version, recommended_version, latest_version
 
 
-def check_min_required_version(required_version) -> bool:
+def check_min_required_version(api_url: str) -> bool:
     """Checks if this package is the same or higher than the minimum required version.
 
     Returns:
         True if the current package version is the same or higher than the minimum required version.
         False with printing a warning message
     """
+    min_version, recommended_version, latest_version = get_versions(api_url)
     current_version = importlib.metadata.version("podonos")
     log.debug(f'current package version: {current_version}')
-    if Version(current_version) >= Version(required_version):
+
+    if Version(current_version) >= Version(recommended_version):
+        # Great!
         return True
-    print(bcolors.WARN + f"The minimum podonos package version is {required_version} while " +
-          f"the current version is {current_version}." + bcolors.ENDC + "\n"
-          "Please upgrade by 'pip install podonos --upgrade'")
+
+    if Version(current_version) >= Version(min_version):
+        # This version is higher than the minimum required version, but less than the recommended
+        # version. Gently recommend to upgrade.
+        print(bcolors.WARN + f"The current podonos package version is {current_version} "
+              f"while a newer version {latest_version} is available" + bcolors.ENDC + "\n" +
+              bcolors.BOLD + "Please upgrade" + bcolors.ENDC + " by "
+              f"'pip install podonos --upgrade'")
+        return True
+
+    # This version is lower than the minimum required version. Cannot proceed.
+    print(bcolors.FAIL + f"The current podonos package version is {current_version} "
+                         f"while the minimum supported version is {min_version}" + bcolors.ENDC + "\n" +
+          bcolors.BOLD + "Please upgrade" + bcolors.ENDC + f" by 'pip install podonos --upgrade'")
     return False
