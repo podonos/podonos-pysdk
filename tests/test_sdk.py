39532547264ebe717632fddaf37ae723743b035e
Soohyun Bae <sb@podonos.com>
2024-06-12 17:25:38 -0700

    fix failed test

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -1,3 +1,4 @@
+import os
 import podonos
 import unittest
 from unittest import mock
@@ -7,26 +8,29 @@ from unittest import mock
 def mocked_requests_get(*args, **kwargs):
     class MockResponse:
         def __init__(self, response_text, response_json, status_code):
-            self.text_response = response_text
+            self.text = response_text
             self.json_response = response_json
             self.status_code = status_code
 
         def json(self):
             return self.json_response
 
-    if '/clients/verify' in args[0]:
+    if '/customers/verify/api-key' in args[0]:
         # API key verification
         return MockResponse('true', None, 200)
 
     if '/version/sdk' in args[0]:
         # SDK versions
         version_response = dict(
-            latest="0.1.0",
-            recommended="0.0.12",
-            minimum="0.0.12")
+            latest="0.1.5",
+            recommended="0.1.4",
+            minimum="0.1.0")
         return MockResponse(None,
                             version_response,
                             200)
+
+    if '/customers/uploading-presigned-url' in args[0]:
+        return MockResponse("https://fake.podonos.com/my_url1", None, 200)
 
     return MockResponse(None, None, 404)
 
@@ -132,5 +136,29 @@ class TestPodonosClient(unittest.TestCase):
         self.assertIsNotNone(etor)
 
 
+class TestPodonosEvaluator(unittest.TestCase):
+
+    @unittest.skip
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_evaluator(self, mock_get):
+        valid_api_key = "1234567890"
+        self._mock_client = podonos.init(api_key=valid_api_key)
+
+        self.config = {
+            'name': 'my_new_model_03272024_p1_k2_en_us',
+            'desc': 'Updated model',
+            'type': 'NMOS',
+            'lan': 'en-us'
+        }
+
+        etor = self._mock_client.create_evaluator(
+            name='new_model', desc='', type='NMOS', lan='en-au')
+        self.assertIsNotNone(etor)
+
+        some_file = os.path.join(os.path.dirname(__file__), '../speech_0_0.mp3')
+        etor.add_file(path=some_file, tag='unknown_file,new_model')
+        etor.close()
+
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
