0bc43de9e9b06521020d9d7b21584b708a185ae2
Jinsung Kim <kim@podonos.com>
2024-06-25 20:17:42 +0900

    fix : add more language

--- podonos/common/enum.py
+++ podonos/common/enum.py
@@ -21,13 +21,19 @@ class EvalType(Enum):
         return f"SPEECH_{self.value}"
 
 class Language(Enum):
-    EN_US = 'en-us'
-    EN_GB = 'en-gb'
-    EN_AU = 'en-au'
-    EN_CA = 'en-ca'
-    ES_ES = 'es-es'
-    ES_MX = 'es-mx'
-    KO_KR = 'ko-kr'
+    ENGLISH_AMERICAN = "en-us"
+    ENGLISH_BRITISH = "en-gb"
+    ENGLISH_AUSTRALIAN = "en-au"
+    ENGLISH_CANADIAN = "en-ca"
+    KOREAN = "ko-kr"
+    MANDARIN = "zh-cn"
+    SPANISH_SPAIN = "es-es"
+    SPANISH_MEXICO = "es-mx"
+    FRENCH = "fr-fr"
+    GERNAM = "de-de"
+    JAPANESE = "ja-jp"
+    ITALIAN = "it-it"
+    POLISH = "pl-pl"
     AUDIO = 'audio'
 
 class QuestionFileType(str, Enum):
