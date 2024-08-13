81e4b75dc50c0d363ce4186caf27e4cfae75cc0c
Soohyun Bae <sb@podonos.com>
2024-08-13 00:15:06 -0700

    remove unused code

--- tests/test_upload_manager.py
+++ tests/test_upload_manager.py
@@ -1,27 +1,13 @@
-import os
 import unittest
-import requests
 
 from datetime import datetime
-from typing import Optional
-from unittest import mock
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import MagicMock
 
-from podonos.common.enum import QuestionFileType
-from podonos.common.exception import HTTPError
-from podonos.core.api import APIClient
-from podonos.core.config import EvalConfig
-from podonos.core.evaluation import Evaluation
-from podonos.core.evaluator import Evaluator
-from podonos.core.file import File
 from podonos.core.upload_manager import UploadManager
 from tests.test_audio import TESTDATA_SPEECH_CH1_MP3
 
 
 class TestUploadManager(unittest.TestCase):
-
-    def setUp(self):
-        print('aa')
 
     def test_upload_manager(self):
         mock_response = MagicMock()
