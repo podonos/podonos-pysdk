77a6e3998879f2935a767e0d89d02a234e9ad728
Soohyun Bae <sb@podonos.com>
2024-08-22 20:42:31 -0700

    base

---
+++ podonos/core/base.py
@@ -0,0 +1,12 @@
+# Base package for every SDK files.
+
+import logging
+import glog as log
+
+
+#logging.basicConfig(level=logging.INFO)
+#log = logging.getLogger(__name__)
+
+log.setLevel("INFO")
+
+
