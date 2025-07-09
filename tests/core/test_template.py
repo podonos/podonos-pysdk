8586a5ffb2f7a258a069ba67a488ef566a6ad8df
CEOJINSUNG <kim@podonos.com>
2025-07-10 08:02:29 +0900

    test : remove use_annotation and use_loudness_normalization

--- tests/core/test_template.py
+++ tests/core/test_template.py
@@ -17,8 +17,6 @@ class TestTemplate(unittest.TestCase):
             "title": "Test Template",
             "description": "Test Description",
             "batch_size": 1,
-            "use_annotation": True,
-            "use_power_normalization": True,
             "language": "en-us",
             "created_time": "2024-03-20T10:00:00Z",
             "updated_time": "2024-03-20T10:30:00Z",
@@ -34,8 +32,6 @@ class TestTemplate(unittest.TestCase):
         self.assertEqual(template.description, "Test Description")
         self.assertEqual(template.batch_size, 1)
         self.assertEqual(template.language, Language.ENGLISH_AMERICAN)
-        self.assertEqual(template.use_annotation, True)
-        self.assertEqual(template.use_loudness_normalization, True)
         self.assertIsInstance(template.created_time, datetime)
         self.assertIsInstance(template.updated_time, datetime)
 
